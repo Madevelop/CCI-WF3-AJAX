8a4e8838259f6d41a0d1412eba5f002d1763687f
Mathilde Ponsar <mathildeponsar@MBP-de-Mathilde.mhs>
2020-03-02 11:17:06 +0100

    ajax.js Instance d'objet XMLHttpRequest

--- index.php
+++ index.php
@@ -9,21 +9,23 @@
 
 <body>
     <script>
-        let xhr = null;
+        function xhr() {
+            let xhr = null;
 
-        if (window.XMLHttpRequest) {
-            xhr = new XMLHttpRequest();
-        } else if (window.ActiveXObject) {
-            try {
-                xhr = new ActiveXObject("Msxml2.XMLHTTP");
+            if (window.XMLHttpRequest) {
+                xhr = new XMLHttpRequest();
+            } else if (window.ActiveXObject) {
+                try {
+                    xhr = new ActiveXObject("Msxml2.XMLHTTP");
 
-            } catch (e) {
-                xhr = new ActiveXObject("Microsoft.XMLHTTP");
+                } catch (e) {
+                    xhr = new ActiveXObject("Microsoft.XMLHTTP");
 
+                }
+            } else {
+                alert("Mettre à jour votre navigateur");
+                xhr = false
             }
-        } else{
-            alert("Mettre à jour votre navigateur");
-            xhr = false
         }
     </script>
 </body>
